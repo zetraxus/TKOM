5cd50d65666cee1576807fe762a97f7f01f94d6e
Adam Kapica <adam.kapica@gmail.com>
2019-05-08 22:37:36 +0200

    Created Makefile with release and tests modes.

---
+++ Makefile
@@ -0,0 +1,4 @@
+release:
+	g++ --std=c++11 src/main.cpp src/instructions/* src/lexer/* src/parser/* -o compile/release
+test:
+	g++ --std=c++11 tests/LexerTests.cpp src/instructions/* src/lexer/* src/parser/* -o compile/tests -lboost_unit_test_framework 
--- tests/LexerTests.cpp
+++ tests/LexerTests.cpp
@@ -5,9 +5,10 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE LexerTests
 #include <boost/test/unit_test.hpp>
-#include "../lexer/Scanner.h"
-#include "../lexer/Source.h"
+#include "../src/lexer/Scanner.h"
+#include "../src/lexer/Source.h"
 
 BOOST_AUTO_TEST_CASE(test1){
+    Scanner* scanner;
     BOOST_CHECK(2 + 2 == 4);
 }
